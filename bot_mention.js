7c1dbbccd31e04dfe7a770227413f1c0a3b0e930
Vlad-Andrei Medintu <80452570+Vladutzu27@users.noreply.github.com>
2022-04-04 15:38:36 +0300

    am corectat niște erori de sintaxă

--- bot_mention.js
+++ bot_mention.js
@@ -1,8 +1,6 @@
 // authenticates you with the API standard library
 const lib = require('lib')({token: process.env.STDLIB_SECRET_TOKEN});
 
-});
-
 let messageResponse = await lib.discord.channels['@0.3.1'].messages.create({
   channel_id: `${context.params.event.channel_id}`,
   content: [
