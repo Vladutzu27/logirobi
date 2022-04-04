fe6fd5132885ca83d273586c8ed9560b6f7893f2
Vlad-Andrei Medintu <80452570+Vladutzu27@users.noreply.github.com>
2022-04-04 15:35:50 +0300

    Create bot_mention.js

---
+++ bot_mention.js
@@ -0,0 +1,35 @@
+// authenticates you with the API standard library
+const lib = require('lib')({token: process.env.STDLIB_SECRET_TOKEN});
+
+lib.discord.users['@0.2.1'].me.status.update({
+  activity_name: `this.help`,
+  activity_type: 'LISTENING',
+  status: 'ONLINE'
+});
+
+let messageResponse = await lib.discord.channels['@0.3.1'].messages.create({
+  channel_id: `${context.params.event.channel_id}`,
+  content: [
+    `Salut, <@!${context.params.event.author.id}>! Sunt un bot făcut de WutWutDiriDiriDam#0069`
+  ].join('\n'),
+  embed: {
+    title: '**Iată ce pot face:**',
+    type: 'rich',
+    color: 0x00AA00, // Green color
+    description: '',
+    fields: [{
+      name: '```this.say("<mesaj>")```',
+      value: [
+        'Spun mesajul impus de tine',
+      ].join('\n')
+    }, {
+      name: '```this.project("<my.logiscool URL, play.scoolcode URL or public project ID>")```',
+      value: [
+        'Arăt detaliile unui proiect, folosind scoolcode API',
+      ].join('\n')
+    }]
+  },
+  tts: false
+});
+
+return messageResponse;
