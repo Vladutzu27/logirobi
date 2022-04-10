2f52d720145ec7cf2d08ac42ea006387a5e80493
Vlad-Andrei Medintu <80452570+Vladutzu27@users.noreply.github.com>
2022-04-10 21:14:58 +0300

    finnaly made it work (kinda)

--- project.js
+++ project.js
@@ -1,44 +1,50 @@
-//This code tells you if you entered a my.logiscool link, a play.scoolcode link or an id, as well as telling you if you typed it with or without https://
-//I will do the fancy algorithms that get things from the scoolcode api later
-
+//Huge improvement
+//Still unfunctional
+//Credit to NopeProsOnly for helping with API crap
 const lib = require('lib')({token: process.env.STDLIB_SECRET_TOKEN});
 
 let projectId = context.params.event.data.options[0].value;
-
+let linkId1 = projectId.substring(projectId.lastIndexOf(`/`) + 1, projectId.indexOf(`?`))
+let linkId2 = projectId.substring(projectId.lastIndexOf(`/`) + 1)
+let linkIndex = 0;
 if(projectId.charAt(0) == "h"){
-  if(projectId.charAt(9) == "p"){
-    projectId = "debug.play"
-  }
-  else{
-    if(projectId.charAt(9) == "m"){
-      projectId = "debug.my"
-    }
-    else{
-      projectId = "debug.err"
-    }
-  }
+  linkIndex = 9
+}
+if(projectId.charAt(linkIndex) == "p"){
+  let result = await lib.http.request['@1.1.6'].get({
+    url: `https://api.prod.scoolcode.com/files/${linkId1}`
+  });
 }
 else{
-  if(projectId.charAt(0) == "p"){
-    projectId = "debug.simple.play"
-  }
-  else{
-    if(projectId.charAt(0) == "m"){
-      projectId = "debug.simple.my"
+  if(projectId.charAt(linkIndex) == "m"){
+    let result = await lib.http.request['@1.1.6'].get({
+      url: `https://api.prod.scoolcode.com/files/${linkId2}`
+    });
     }
     else{
-      if(projectId.length == 24){
-        projectId = "debug.id"
+      let result3 = await lib.http.request['@1.1.6']({
+        url: `https://api.prod.scoolcode.com/files/${projectId}`
+      });
       }
-      else{
-        projectId = "debug.simple.err"
-      }
-    }
+let test = 'test'
+console.log(`${test}`);
+if (projectId.includes(`?`)){
+  if (linkId1.length != 24) {
+    console.log(result1.data)
+  } else {
+    console.log('Not 24 char long')
   }
-}
+} else {
+  if (linkId2.length != 24) {
+    console.log(result2.data)
+  } else {
+    console.log('Not 24 char long')
+  }
 
 
-await lib.discord.channels['@0.0.6'].messages.create({
-  channel_id: context.params.event.channel_id,
-  content: `debug ->  ${projectId}` 
-});
+
+
+  await lib.discord.channels['@0.0.6'].messages.create({
+    channel_id: context.params.event.channel_id,
+    content: `result.data.cookieCounter`
+  });
